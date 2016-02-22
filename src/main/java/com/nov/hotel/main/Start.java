1239441a4379bc7d14daecfac4518a07d58a884f
IgorNo <novikof1@inbox.ru>
2016-02-22 10:33:19 +0200

    Add Locale

--- src/main/java/com/nov/hotel/main/Start.java
+++ src/main/java/com/nov/hotel/main/Start.java
@@ -6,31 +6,19 @@ import com.nov.hotel.dao.objects.MP3;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Locale;
+
 public class Start {
 
+	private static final Locale DEFAULT_LOCALE = new Locale("ua");
+
 	public static void main(String[] args) {
-		MP3 firstMP3 = new MP3();
-		firstMP3.setName("Song N55");
 
-		Author author = new Author();
-		author.setName("Justin");
+		Locale.setDefault(DEFAULT_LOCALE);
 
-		firstMP3.setAuthor(author);
-		//
-		// MP3 secondMP3 = new MP3();
-		// secondMP3.setName("Song name2");
-		// secondMP3.setAuthor("Song author2");
-		//
-		// List<MP3> list = new ArrayList<>();
-		//
-		// list.add(firstMP3);
-		// list.add(secondMP3);
-		//
-		// // new SQLiteDAO().insertWithJDBC(mp3);
-		//
 		ApplicationContext context = new ClassPathXmlApplicationContext("/spring/app-context.xml");
-		AppartTypeDao sqLiteDAO = (AppartTypeDao) context.getBean("appartTypeDao");
 
+		AppartTypeDao sqLiteDAO = (AppartTypeDao) context.getBean("appartTypeDao");
 		System.out.println(sqLiteDAO.getAll());
 
 	}
