bc276f369fb1f4e459331e5fbdf05096f9fab488
IgorNo <novikof1@inbox.ru>
2016-03-15 12:17:02 +0200

    Refactoring Collections (Singelton)

--- src/main/java/com/nov/hotel/collections/impl/ApartmentCollection.java
+++ src/main/java/com/nov/hotel/collections/impl/ApartmentCollection.java
@@ -1,13 +1,25 @@
 package com.nov.hotel.collections.impl;
 
-import com.nov.hotel.collections.abstr.ObservaableCollectionAbstract;
+import com.nov.hotel.collections.abstr.ObservableCollectionAbstract;
 import com.nov.hotel.dao.interfaces.CrudDao;
 import com.nov.hotel.entities.Apartment;
 import com.nov.hotel.main.Start;
 
-public class ApartmentCollection extends ObservaableCollectionAbstract<Apartment> {
+public class ApartmentCollection extends ObservableCollectionAbstract<Apartment> {
 
     private CrudDao<Apartment> dao = (CrudDao<Apartment>) Start.APPLICATION_CONTEXT.getBean("appartmentDao");
+
+    private static ObservableCollectionAbstract uniqueObsColl;
+
+    private ApartmentCollection() {
+    }
+
+    public static ObservableCollectionAbstract getInstance() {
+        if (uniqueObsColl == null){
+            uniqueObsColl = new ApartmentCollection();
+        }
+        return uniqueObsColl;
+    }
 
     @Override
     protected CrudDao getDao() {
